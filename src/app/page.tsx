3d2001cc039b733989a5389b7e3cc4d7fd86e33d
zain-shehzad <zainshehzad5656@gmail.com>
2024-09-01 22:49:38 +0500

    change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import React from "react"
-
+import Image from "next/image"
 const page = () => {
   return (
-    <div>home page</div>
+    <div>home page
+      <Image src="src/house.jfif" alt="alt" width={500} height={500} />
+    </div>
   )
 }
 
